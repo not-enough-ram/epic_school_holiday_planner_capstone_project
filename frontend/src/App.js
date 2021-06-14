9a09c636dfea4f6c59765aff8b526f906d945090
Jonathan <jonathan.heyer@gmail.com>
2021-06-14 21:55:27 +0200

    added landing page for testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import {Route, Switch} from "react-router-dom";
 import './App.css';
+import NavHeader from "./components/NavHeader";
+import LoginPage from "./Pages/LoginPage";
+import {ThemeProvider} from '@primer/components'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <ThemeProvider>
+        <NavHeader/>
+        <Switch>
+          <Route path={'/'} exact>
+            <LoginPage/>
+          </Route>
+        </Switch>
+      </ThemeProvider>
+  )
 }
 
 export default App;
