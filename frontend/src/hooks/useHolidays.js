1887e4fd3606637cbeebc82c6da501bec4b1c5ea
Jonathan <jonathan.heyer@gmail.com>
2021-06-16 16:27:53 +0200

    added custom hook to fetch holidays from backend

---
+++ frontend/src/hooks/useHolidays.js
@@ -0,0 +1,24 @@
+import {useContext, useEffect, useState} from "react";
+import axios from "axios";
+import AuthContext from "../context/AuthContext";
+
+export default function useHolidays(user) {
+
+    const [holidays, setHolidays] = useState([]);
+    const {token} = useContext(AuthContext)
+
+    useEffect(() => {
+        const config = {
+            headers: {
+                Authorization: "Bearer " + token,
+            },
+        }
+        axios
+            .get(`/api/holidays`, config)
+            .then((response) => response.data)
+            .then(setHolidays)
+            .catch((error) => console.error(error.message));
+    }, [user, token]);
+
+    return { holidays };
+}
