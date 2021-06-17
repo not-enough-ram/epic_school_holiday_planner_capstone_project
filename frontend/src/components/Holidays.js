e27321a7446e0b13c0766fe424219a8cf0e48df7
Jonathan Heyer <54237352+not-enough-ram@users.noreply.github.com>
2021-06-17 15:54:41 +0200

    did some styling

--- frontend/src/components/Holidays.js
+++ frontend/src/components/Holidays.js
@@ -1,15 +1,17 @@
 import styled from "styled-components/macro";
 
-export default function Holidays({holidays}) {
-    return (
-        <Holiday>
-            <h1>{holidays.name}</h1>
-            <h2>{holidays.startDate}</h2>
-            <h2>{holidays.endDate}</h2>
-        </Holiday>
-    )
+export default function Holidays({ holidays }) {
+  return (
+    <Holiday>
+      <h1>{holidays.name}</h1>
+      <h3>{holidays.startDate}</h3>
+      <h3>{holidays.endDate}</h3>
+    </Holiday>
+  );
 }
 
 const Holiday = styled.section`
   background: darkgrey;
+  border: black thin;
+  border-radius: 25px;
 `;
