a311669d226ed4d2c10a416545ae9ef4d520adde
Jonathan Heyer <54237352+not-enough-ram@users.noreply.github.com>
2021-06-18 10:00:24 +0200

    fixed dev dependencies, added frontend components for details and booking, trying to get the datepicker to work

--- backend/src/main/java/de/neuefische/backend/repository/BookedHolidaysRepository.java
+++ backend/src/main/java/de/neuefische/backend/repository/BookedHolidaysRepository.java
@@ -1,7 +1,6 @@
 package de.neuefische.backend.repository;
 
 import de.neuefische.backend.model.BookedHolidays;
-import de.neuefische.backend.model.Holidays;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
