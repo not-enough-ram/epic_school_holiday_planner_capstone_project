14b31171472f2d5af72d6a82685d5565746df803
Jonathan Heyer <54237352+not-enough-ram@users.noreply.github.com>
2021-07-06 10:54:42 +0200

    show all users page ready

--- frontend/src/Pages/AllUsersPage.js
+++ frontend/src/Pages/AllUsersPage.js
@@ -1,17 +1,35 @@
 import useAllAppUsers from "../hooks/useAllAppUsers";
 import { Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  root: {
+    paddingTop: 10,
+    display: "flex",
+    flexFlow: "column nowrap",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+    width: "100%",
+    height: "100vh",
+    textAlign: "left",
+  },
+  user: {
+    marginLeft: 10,
+  },
+});
 
 export default function AllUsersPage() {
+  const classes = useStyles();
   const { data: appUsers, isLoading, error } = useAllAppUsers();
   if (isLoading) return "is loading ...";
   if (error) return "something went wrong";
   return (
-    <section>
+    <section className={classes.root}>
       {appUsers &&
         appUsers.map((user) => (
-          <section>
-            <Typography>{user?.username}</Typography>
-            <Typography>{user?.role}</Typography>
+          <section className={classes.user}>
+            <Typography>Login: {user?.username}</Typography>
+            <Typography>Rolle: {user?.role}</Typography>
             <p>- - - -</p>
           </section>
         ))}
